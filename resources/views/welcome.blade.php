dcf98178a5778d9c1e60dece45f853b5024b725b
Jarkko Heikkinen <jarkko.heikkinen@mediasignal.fi>
2018-04-05 14:36:54 +0300

    Created a common form for creating and updating Contacts. #11:62

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-    <div class="container">
+    <div class="container mt-4">
         <div id="app">
             <contacts></contacts>
 
