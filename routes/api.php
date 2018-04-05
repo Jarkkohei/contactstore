d0104dfaa557eb39abf33255af0a83e97e538011
Jarkko Heikkinen <jarkko.heikkinen@mediasignal.fi>
2018-04-05 13:39:02 +0300

    Added the API routes and the functionalities for them without any controllers. #11:61

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Contact;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +13,41 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::group(['middleware' => 'api'], function() {
+    //  Fetch contacts
+    Route::get('contacts', function() {
+        return Contact::latest()->orderBy('created_at', 'desc')->get();
+    });
+
+    //  Get single contact
+    Route::get('contact/{id}', function($id) {
+        return Contact::findOrFail($id);
+    });
+
+    //  Add contact
+    Route::post('contact/store', function(Request $request) {
+        return Contact::create([
+            'name' => $request->input(['name']), 
+            'email' => $request->input(['email']), 
+            'phone' => $request->input(['phone'])
+        ]);
+    });
+
+    //  Update contact
+    Route::patch('contact/{id}', function(Request $request, $id) {
+        Contact::findOrFail($id)->update([
+            'name' => $request->input(['name']), 
+            'email' => $request->input(['email']), 
+            'phone' => $request->input(['phone'])
+        ]);
+    });
+
+    //  Delete contact
+    Route::delete('contact/{id}', function($id) {
+        return Contact::destroy($id);
+    });
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
